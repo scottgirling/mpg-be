60db419981550df07c6d5b4d6b5eecfc44d6e571
scottgirling <scottgirling0812@gmail.com>
2025-04-24 13:58:50 +0100

    add test and development data

---
+++ db/data/test-data/users.js
@@ -0,0 +1,162 @@
+module.exports = [
+	{
+		"username": "chef_anna",
+		"name": "Anna",
+		"avatar_url": "https://picsum.photos/200?random=1",
+		"meal_plans": [
+			{
+				"2025-04-21": "Shrimp Tacos",
+				"2025-04-22": "Vegan Buddha Bowl",
+				"2025-04-23": "Spaghetti Carbonara",
+				"2025-04-24": "Tom Yum Soup",
+				"2025-04-25": "Banana Bread",
+				"2025-04-26": "Caesar Salad",
+				"2025-04-27": "Classic Pancakes"
+			},
+			{
+				"2025-05-01": "Greek Yogurt Parfait",
+				"2025-05-02": "Beef Stir Fry",
+				"2025-05-03": "Miso Ramen",
+				"2025-05-04": "Chicken Tikka Masala"
+			}
+		],
+		"favourite_meals": [
+			"Spaghetti Carbonara",
+			"Banana Bread",
+			"Shrimp Tacos"
+		]
+	},
+	{
+		"username": "spice_master",
+		"name": "Master Spicer",
+		"avatar_url": "https://picsum.photos/200?random=2",
+		"meal_plans": [
+			{
+				"2025-04-21": "Chicken Tikka Masala",
+				"2025-04-22": "Beef Stir Fry",
+				"2025-04-23": "Tom Yum Soup",
+				"2025-04-24": "Miso Ramen",
+				"2025-04-25": "Vegan Buddha Bowl",
+				"2025-04-26": "Caprese Salad",
+				"2025-04-27": "Shrimp Tacos"
+			},
+			{
+				"2025-05-01": "Spaghetti Carbonara",
+				"2025-05-02": "Greek Yogurt Parfait",
+				"2025-05-03": "Banana Bread",
+				"2025-05-04": "Classic Pancakes"
+			}
+		],
+		"favourite_meals": [
+			"Chicken Tikka Masala",
+			"Shrimp Tacos",
+			"Spaghetti Carbonara"
+		]
+	},
+	{
+		"username": "plant_power",
+		"name": "Power Plant",
+		"avatar_url": "https://picsum.photos/200?random=3",
+		"meal_plans": [
+			{
+				"2025-04-21": "Greek Yogurt Parfait",
+				"2025-04-22": "Vegan Buddha Bowl",
+				"2025-04-23": "Chicken Tikka Masala",
+				"2025-04-24": "Beef Stir Fry",
+				"2025-04-25": "Greek Yogurt Parfait",
+				"2025-04-26": "Banana Bread",
+				"2025-04-27": "Shrimp Tacos"
+			},
+			{
+				"2025-05-01": "Classic Pancakes",
+				"2025-05-02": "Caprese Salad",
+				"2025-05-03": "Miso Ramen",
+				"2025-05-04": "Spaghetti Carbonara"
+			}
+		],
+		"favourite_meals": [
+			"Vegan Buddha Bowl",
+			"Caesar Salad",
+			"Greek Yogurt Parfait"
+		]
+	},
+	{
+		"username": "breakfast_queen",
+		"name": "Queen of Breakfast",
+		"avatar_url": "https://picsum.photos/200?random=4",
+		"meal_plans": [
+			{
+				"2025-04-21": "Classic Pancakes",
+				"2025-04-22": "Banana Bread",
+				"2025-04-23": "Chicken Tikka Masala",
+				"2025-04-24": "Miso Ramen",
+				"2025-04-25": "Vegan Buddha Bowl",
+				"2025-04-26": "Greek Yogurt Parfait",
+				"2025-04-27": "Tom Yum Soup"
+			},
+			{
+				"2025-05-01": "Spaghetti Carbonara",
+				"2025-05-02": "Beef Stir Fry",
+				"2025-05-03": "Shrimp Tacos",
+				"2025-05-04": "Banana Bread"
+			}
+		],
+		"favourite_meals": [
+			"Classic Pancakes",
+			"Banana Bread"
+		]
+	},
+	{
+		"username": "noodle_ninja",
+		"name": "Ninja Noodle",
+		"avatar_url": "https://picsum.photos/200?random=5",
+		"meal_plans": [
+			{
+				"2025-04-21": "Miso Ramen",
+				"2025-04-22": "Beef Stir Fry",
+				"2025-04-23": "Tom Yum Soup",
+				"2025-04-24": "Shrimp Tacos",
+				"2025-04-25": "Greek Yogurt Parfait",
+				"2025-04-26": "Vegan Buddha Bowl",
+				"2025-04-27": "Spaghetti Carbonara"
+			},
+			{
+				"2025-05-01": "Caprese Salad",
+				"2025-05-02": "Caesar Salad",
+				"2025-05-03": "Banana Bread",
+				"2025-05-04": "Classic Pancakes"
+			}
+		],
+		"favourite_meals": [
+			"Miso Ramen",
+			"Tom Yum Soup",
+			"Spaghetti Carbonara"
+		]
+	},
+	{
+		"username": "fresh_flavors",
+		"name": "Fresh Flavors",
+		"avatar_url": "https://picsum.photos/200?random=6",
+		"meal_plans": [
+			{
+				"2025-04-21": "Greek Yogurt Parfait",
+				"2025-04-22": "Caesar Salad",
+				"2025-04-23": "Banana Bread",
+				"2025-04-24": "Tom Yum Soup",
+				"2025-04-25": "Chicken Tikka Masala",
+				"2025-04-26": "Spaghetti Carbonara",
+				"2025-04-27": "Miso Ramen"
+			},
+			{
+				"2025-05-01": "Shrimp Tacos",
+				"2025-05-02": "Vegan Buddhe Bowl",
+				"2025-05-03": "Beef Stir Fry",
+				"2025-05-04": "Caesar Salad"
+			}
+		],
+		"favourite_meals": [
+			"Greek Yogurt Parfait",
+			"Shrimp Tacos"
+		]
+	}
+];  
