51a3bac0a95e60b7d33a6f812dad439f84b9b2cd
scottgirling <scottgirling0812@gmail.com>
2025-04-30 11:32:10 +0100

    update /api/recipes/:recipe_id to remove util func and move code into model

--- controllers/getRecipeById.js
+++ controllers/getRecipeById.js
@@ -1,12 +1,8 @@
 const selectRecipeById = require("../models/selectRecipeById");
-const checkRecipeExists = require("../models/utils/checkRecipeExists");
 
 const getRecipeById = (request, response, next) => {
     const { recipe_id } = request.params;
-    checkRecipeExists(recipe_id)
-    .then(() => {
-        return selectRecipeById(recipe_id)
-    })
+    selectRecipeById(recipe_id)
     .then((recipe) => {
         response.status(200).send({ recipe });
     })
