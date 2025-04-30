316afbbf3ee0650394bfa4081560bfa06b04fa01
scottgirling <scottgirling0812@gmail.com>
2025-04-30 18:25:52 +0100

    add GET /api/recipes QUERIES with tests

--- controllers/getRecipes.js
+++ controllers/getRecipes.js
@@ -1,13 +1,29 @@
 const selectRecipes = require("../models/selectRecipes");
+const checkTagExists = require("../models/utils/checkTagsExist");
 
 const getRecipes = (request, response, next) => {
-    selectRecipes()
-    .then((recipes) => {
-        response.status(200).send({ recipes });
-    })
-    .catch((error) => {
-        next(error);
-    });
+    const { sort_by, order, tags } = request.query;
+    if (tags) {
+        checkTagExists(tags)
+        .then(() => {
+            return selectRecipes(sort_by, order, tags)
+        })
+        .then((recipes) => {
+            response.status(200).send({ recipes });
+        })
+        .catch((error) => {
+            next(error);
+        });
+    } else {
+        selectRecipes(sort_by, order, tags)
+        .then((recipes) => {
+            response.status(200).send({ recipes });
+        })
+        .catch((error) => {
+            next(error);
+        });
+    }
+
 }
 
 module.exports = getRecipes;
--- models/selectRecipes.js
+++ models/selectRecipes.js
@@ -1,8 +1,31 @@
 const db = require("../db/connection");
 
-const selectRecipes = () => {
-    return db.query("SELECT * FROM recipes;")
+const selectRecipes = (sort_by = "created_at", order = "desc", tags) => {
+    const allowedSortBy = ["created_at", "votes", "difficulty", "prep_time", "cook_time"];
+    const allowedOrderBy = ["asc", "desc"];
+    const queryValues = [];
+
+    if (!allowedSortBy.includes(sort_by) || !allowedOrderBy.includes(order)) {
+        return Promise.reject({ status: 400, msg: "Invalid 'Sort By' or 'Order'. Please select a valid input." });
+    }
+
+    let sqlStr = 'SELECT * FROM recipes';
+
+    if (tags) {
+        if (typeof tags === "string") {
+            sqlStr += ' WHERE tags[1] = $1 OR tags[2] = $1 OR tags[3] = $1';
+            queryValues.push(tags);
+        } else if (tags.length > 1) {
+            sqlStr += ' WHERE tags[1] = $1 OR tags[2] = $1 OR tags[3] = $1 OR tags[1] = $2 OR tags[2] = $2 OR tags[3] = $2 OR tags[1] = $3 OR tags[2] = $3 OR tags[3] = $3';
+            tags.forEach((tag) => queryValues.push(tag));
+        }
+    }
+
+    sqlStr += ` ORDER BY ${sort_by} ${order}`;
+    
+    return db.query(sqlStr, queryValues)
     .then(({ rows }) => {
+        // console.log(rows, "<<<<,")
         return rows;
     });
 }
