e74b8c5daa2d5d607ff3a571d557b5eebfe7f9ec
scottgirling <scottgirling0812@gmail.com>
2025-05-07 12:56:21 +0100

    amend util func to fulfil new query requirement

--- models/utils/checkTagsExist.js
+++ models/utils/checkTagsExist.js
@@ -2,20 +2,15 @@ const db = require("../../db/connection");
 
 const checkTagExists = (tags) => {
     if (Array.isArray(tags)) {
-        return db.query("SELECT * FROM tags WHERE tags.slug = $1 OR tags.slug = $2 OR tags.slug = $3", tags)
-        .then(({ rows }) => {
-            if (!rows.length) {
-                return Promise.reject({ status: 404, msg: "Category does not exist. Please try another." });
-            }
-        });
-    } else {
-        return db.query("SELECT * FROM tags WHERE tags.slug = $1", [tags])
-        .then(({ rows }) => {
-            if (!rows.length) {
-                return Promise.reject({ status: 404, msg: "Category does not exist. Please try another." });
-            }
-        });
+        return Promise.reject({ status: 400, msg: "Invalid request - select a single tag." });
     }
+    
+    return db.query("SELECT * FROM tags WHERE tags.slug = $1", [tags])
+    .then(({ rows }) => {
+        if (!rows.length) {
+            return Promise.reject({ status: 404, msg: "Category does not exist. Please try another." });
+        }
+    });
 }
 
 module.exports = checkTagExists;
