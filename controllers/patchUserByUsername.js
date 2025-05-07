2dce053c07ea61ca542e588a40c1b5999d13c037
scottgirling <scottgirling0812@gmail.com>
2025-05-07 10:51:08 +0100

    add PATCH /api/users/:username (meal_plans) endpoint

--- controllers/patchUserByUsername.js
+++ controllers/patchUserByUsername.js
@@ -2,17 +2,17 @@ const updateUserByUsername = require("../models/updateUserByUsername");
 
 const patchUserByUsername = (request, response, next) => {
     const { username } = request.params;
-    const { favouriteMealChange } = request.body;
+    const { favouriteMealChange, mealPlanChange } = request.body;
 
-    if (typeof favouriteMealChange === "number") {
-        updateUserByUsername(username, favouriteMealChange)
+    if (typeof favouriteMealChange === "number" || typeof mealPlanChange === "object") {
+        updateUserByUsername(favouriteMealChange, mealPlanChange, username)
         .then((user) => {
             response.status(200).send({ user });
         })
         .catch((error) => {
             next(error);
         });
-    } else if (typeof favouriteMealChange === "string") {
+    } else if (typeof favouriteMealChange === "string" || typeof mealPlanChange === "number" || typeof mealPlanChange === "string") {
         return Promise.reject({ status: 400, msg: "Invalid data type." });
     } else {
         return Promise.reject({ status: 400, msg: "Invalid request - missing field(s)." });
